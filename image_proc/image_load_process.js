34074964181626ce77f25ed6d7617bf82e51b033
sklykov <sergej.klykow@gmail.com>
2023-03-28 18:26:21 +0200

    Checking the type of an uploaded file

--- image_proc/image_load_process.js
+++ image_proc/image_load_process.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const readerImg = new FileReader();  // IO API from JS
     let imageClass = new Image();  // Image class for providing raster image to the canvas context drawing
     let widthSet = "55%"; widthInput.value = 55;  // default width setting
-    let imageUploaded = false;
+    let imageUploaded = false;  // flag for referring if image uploaded or not
+    let fileType;  // store the uploaded type file provided in FileReader.result of method readAsDataURL
 
     uploadButton.value = "";  // put default "No file selected" to the input button
 
@@ -24,30 +25,56 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     // Event listener that reacts to finished loaded event from above (readerImg.readAsDataURL(...))
     readerImg.addEventListener("load", (event) => {
-        imgElement.src = readerImg.result;  // transfer loaded image to the HTML element, evoke "load" event in the end of loading
-        // Add event listener to finished load of an image
-        imgElement.onload = () => {
-            console.log(imgElement);
-            canvas.width = imgElement.width; canvas.height = imgElement.height;  // make canvas to inherit geometrical properties of <img> element
-            console.log(`Image WxH: ${imgElement.width}x${imgElement.height}`);
-            // Set now styling for restrict image sizes
-            imgElement.style.width = widthSet; imgElement.style.height = "auto";  // to prevent wrong transfer to canvas file
-            imageClass.src = readerImg.result;  // transfer loaded image to the Image class and can be bound with the "load" event below
-            imageClass.onload = () => {
-                contextCanvas.drawImage(imageClass, 0, 0);  // 0, 0 - coordinates should be provided, it's origin of drawing. This function draws a raster image on canvas
-                canvas.style.width = widthSet; canvas.style.height = "auto";  // same styling as <img> element
-                imageUploaded = true;
+        imageUploaded = false; widthInput.disabled = true; // default values - before it will be confirmed that the image loaded
+        // console.log(readerImg.result.slice(5, 10));  // will print out the type of the uploaded file
+        fileType = readerImg.result.slice(5, 10);
+        // Simple uploaded type checker - if the FileReader reports that uploaded image, then proceed to drawing of its content
+        if (fileType == "image"){
+            imgElement.src = readerImg.result;  // transfer loaded image to the HTML element, evoke "load" event in the end of loading
+            // Add event listener to finished load of an image
+            imgElement.onload = () => {
+                console.log(imgElement);
+                canvas.width = imgElement.width; canvas.height = imgElement.height;  // make canvas to inherit geometrical properties of <img> element
+                console.log(`Image WxH: ${imgElement.width}x${imgElement.height}`);
+                // Set now styling for restrict image sizes
+                imgElement.style.width = widthSet; imgElement.style.height = "auto";  // to prevent wrong transfer to the canvas element
+                imageClass.src = readerImg.result;  // transfer loaded image to the Image class and can be bound with the "load" event below
+                imageClass.onload = () => {
+                    contextCanvas.drawImage(imageClass, 0, 0);  // 0, 0 - coordinates should be provided, it's origin of drawing. This function draws a raster image on canvas
+                    canvas.style.width = widthSet; canvas.style.height = "auto";  // same styling as <img> element
+                    // check that it was normal image (width and height more than 1 pixel)
+                    if ((Number.parseInt(imgElement.width) > 1) && (Number.parseInt(imgElement.height) > 1)){
+                        changePropsImgUploaded();  // centrally changing of properties when the image is uploaded
+                    } else {
+                        window.alert("The uploaded image width and / or height isn't more than 1 pixel, it will be ignored!");
+                    }
+                };
             };
-        }; 
-        uploadInfoStr.innerText = `Image uploaded and shown below. Name of image: ${uploadButton.files[0].name}. Upload new image: `;
-        // imgElement.style.filter = "blur(2px)";
+        } else {
+            clearCanvas();
+        }
+        // setTimeout(750, clearCanvas());  // clear image on canvas if it hasn't been uploaded 
     });
 
-    // Listen to change of width input
+    // Listen to a change on the page of the width input
     widthInput.addEventListener("change", () => {
         if (imageUploaded){
             widthSet = `${widthInput.value}%`;
             canvas.style.width = widthSet; imgElement.style.width = widthSet; 
         }
     });
+
+    // Change properties of page elements if the image was successfully uploaded to the browser
+    function changePropsImgUploaded(){
+        imageUploaded = true; widthInput.disabled = false;
+        uploadInfoStr.innerText = `Image uploaded and shown below. Name of image: ${uploadButton.files[0].name}. Upload new image: `;
+    };
+
+    // Remove previously uploaded image from <img> and <canvas> elements
+    function clearCanvas(){
+        if (!imageUploaded){
+            contextCanvas.clearRect(0, 0, canvas.width, canvas.height);
+            imgElement.src = "";
+        }
+    };
 })
